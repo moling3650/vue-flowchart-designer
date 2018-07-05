633db35a12d602df5d05ce456b75d6389e0b484c
moling <365024424@qq.com>
2018-07-06 07:51:04 +0800

    定义节点的样式

--- src/App.vue
+++ src/App.vue
@@ -30,4 +30,26 @@ div, input, textarea, ul {
   outline: 0;
   font-size: inherit;
 }
+.node {
+  border: 1px solid #333;
+  margin: 0 auto;
+}
+.start {
+  width: 100px;
+  height: 70px;
+  border-radius: 50%;
+}
+.question {
+  width: 50px;
+  height: 50px;
+  transform: rotate(-45deg);
+}
+.action {
+  width: 120px;
+  height: 120px;
+}
+.output {
+  width: 100px;
+  height: 50px;
+}
 </style>
--- src/components/ShapePanel.vue
+++ src/components/ShapePanel.vue
@@ -1,5 +1,18 @@
 <template>
-  <div id="shape_panel" :style="{ height }"></div>
+  <div id="shape_panel" :style="{ height }">
+    <div class="panel_container">
+      <div class="node start"></div>
+    </div>
+    <div class="panel_container">
+      <div class="node question"></div>
+    </div>
+    <div class="panel_container">
+      <div class="node action"></div>
+    </div>
+    <div class="panel_container">
+      <div class="node output"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -26,5 +39,11 @@ export default {
   position: absolute;
   background: #f6f6f6;
   border-right: 1px solid #a2a2a2;
+}
+
+.panel_container {
+  padding: 10px 0;
+  border-bottom: 1px solid #cbcccc;
+  margin-top: 1px;
 }
 </style>
