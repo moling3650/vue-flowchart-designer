6973ffb4da2a6c57668e69fd382956f2bec1359f
moling <365024424@qq.com>
2018-07-10 18:26:08 +0800

    更新样式

--- src/App.vue
+++ src/App.vue
@@ -31,25 +31,68 @@ div, input, textarea, ul {
   font-size: inherit;
 }
 .node {
-  border: 1px solid #333;
-  margin: 0 auto;
+  padding: 16px;
+  position: absolute;
+  z-index: 4;
+  border: 1px solid #2e6f9a;
+  box-shadow: 2px 2px 19px #e0e0e0;
+  -o-box-shadow: 2px 2px 19px #e0e0e0;
+  -webkit-box-shadow: 2px 2px 19px #e0e0e0;
+  -moz-box-shadow: 2px 2px 19px #e0e0e0;
+  -moz-border-radius: 8px;
+  border-radius: 8px;
+  opacity: 0.8;
+  cursor: move;
+  background-color: white;
+  font-size: 11px;
+  -webkit-transition: background-color 0.25s ease-in;
+  -moz-transition: background-color 0.25s ease-in;
+  transition: background-color 0.25s ease-in;
 }
-.start {
-  width: 100px;
-  height: 70px;
-  border-radius: 50%;
+
+.node:hover {
+  background-color: #5c96bc;
+  color: white;
 }
-.question {
-  width: 50px;
-  height: 50px;
-  transform: rotate(-45deg);
+.node-text {
+  margin-right: 10px;
 }
-.action {
-  width: 120px;
-  height: 120px;
+
+.alabel {
+  -webkit-transition: background-color 0.25s ease-in;
+  -moz-transition: background-color 0.25s ease-in;
+  transition: background-color 0.25s ease-in;
 }
-.output {
-  width: 100px;
-  height: 50px;
+
+.alabel.jtk-hover, .jtk-source-hover, .jtk-target-hover {
+  background-color: #1e8151;
+  color: white;
+}
+
+.alabel {
+  background-color: white;
+  opacity: 0.8;
+  padding: 0.3em;
+  border-radius: 0.5em;
+  border: 1px solid #346789;
+  cursor: pointer;
+}
+
+.ep {
+  position: absolute;
+  bottom: 37%;
+  right: 5px;
+  width: 1em;
+  height: 1em;
+  background-color: orange;
+  cursor: pointer;
+  box-shadow: 0 0 2px black;
+  -webkit-transition: -webkit-box-shadow 0.25s ease-in;
+  -moz-transition: -moz-box-shadow 0.25s ease-in;
+  transition: box-shadow 0.25s ease-in;
+}
+
+.ep:hover {
+  box-shadow: 0 0 6px black;
 }
 </style>
