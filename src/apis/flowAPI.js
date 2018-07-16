48258fbb4842ae89dc1aa05b92a10f1aef3af2f4
moling <365024424@qq.com>
2018-07-16 09:35:44 +0800

    添加获取工序组和工序的API

--- src/apis/flowAPI.js
+++ src/apis/flowAPI.js
@@ -15,5 +15,11 @@ export default {
     const sql = `UPDATE B_Process_Flow_Detail SET process_result = (CASE WHEN process_result = 'OK' THEN 'NG' ELSE 'OK' END)
       WHERE flow_code = @fCode AND process_from = @pCode`
     return apis.executeSQL(sql, payload)
+  },
+  fetchProcessGroup () {
+    return apis.executeSQL('SELECT group_code, group_name FROM B_WorkGroup')
+  },
+  fetchProcessByGroup (gCode) {
+    return apis.executeSQL('SELECT process_code, process_name FROM B_ProcessList WHERE group_code = @gCode', { gCode })
   }
 }
