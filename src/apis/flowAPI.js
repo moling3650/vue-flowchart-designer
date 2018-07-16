67c715d6c9b1a6405554d5532354f5d45b60a88c
moling <365024424@qq.com>
2018-07-16 11:47:01 +0800

    更新获取工序组的API

--- src/apis/flowAPI.js
+++ src/apis/flowAPI.js
@@ -16,10 +16,18 @@ export default {
       WHERE flow_code = @fCode AND process_from = @pCode`
     return apis.executeSQL(sql, payload)
   },
-  fetchProcessGroup () {
+  fetchWorkGroup () {
     return apis.executeSQL('SELECT group_code, group_name FROM B_WorkGroup')
   },
-  fetchProcessByGroup (gCode) {
-    return apis.executeSQL('SELECT process_code, process_name FROM B_ProcessList WHERE group_code = @gCode', { gCode })
+  fetchProcessList () {
+    return apis.executeSQL('SELECT group_code, process_code, process_name FROM B_ProcessList ORDER BY idx')
+  },
+  fetchProcessGroup () {
+    return Promise.all([this.fetchWorkGroup(), this.fetchProcessList()]).then(([group, processList]) => {
+      group.forEach(g => {
+        g.processList = processList.filter(p => p.group_code === g.group_code)
+      })
+      return group
+    })
   }
 }
